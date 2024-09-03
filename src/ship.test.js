5f51c3589fd8b8b5de5b1e96ec5aebef6ddbbe03
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-09-03 16:29:01 +0200

    Add a class to create ships and its tests

---
+++ src/ship.test.js
@@ -0,0 +1,33 @@
+import Ship from "./ship";
+
+describe("Ship features", () => {
+  let submarine;
+
+  beforeEach(() => {
+    submarine = new Ship("Submarine", 3);
+  });
+
+  test("Ship has been hit", () => {
+    submarine.hit();
+    expect(submarine.hits).toBe(1);
+  });
+
+  test("Ship has been hit twice", () => {
+    submarine.hit();
+    submarine.hit();
+    expect(submarine.hits).toBe(2);
+  });
+
+  test("Ship is not sunk", () => {
+    submarine.hit();
+    submarine.hit();
+    expect(submarine.isSunk()).toBe(false);
+  });
+
+  test("Ship is sunk", () => {
+    submarine.hit();
+    submarine.hit();
+    submarine.hit();
+    expect(submarine.isSunk()).toBe(true);
+  });
+});
